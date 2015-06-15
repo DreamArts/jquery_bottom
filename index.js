d81d6339a68135acd3ccef0c9a7bc5a32130c5be
exabugs <exabugs@gmail.com>
2015-06-15 10:46:52 +0900

    webpack extension

---
+++ index.js
@@ -0,0 +1,17 @@
+/**
+ * jQuery.bottom
+ * Dual licensed under MIT and GPL.
+ * Date: 2010-04-25
+ *
+ * @description Trigger the bottom event when the user has scrolled to the bottom of an element
+ * @author Jim Yi
+ * @version 1.0
+ *
+ * @id jQuery.fn.bottom
+ * @param {Object} settings Hash of settings.
+ * @return {jQuery} Returns the same jQuery object for chaining.
+ *
+ */
+"use strict";
+
+module.exports = require("./jquery.bottom-1.0.js");
